d81549f00bdb82be2acfac19e55c75ba309a3859
sakiii999 <sakethgaruda@gmail.com>
2017-03-01 18:14:09 -0600

    Lab Assignment 6 Added
    Watch wear app is added

---
+++ Lab Assignment 6/Source/Android Wear App/LocateMe/src/main/java/com/example/wearapp/MainActivity.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.wearapp;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.support.wearable.view.DismissOverlayView;
+import android.view.View;
+import android.view.WindowInsets;
+import android.widget.FrameLayout;
+
+public class MainActivity extends WearableActivity implements OnMapReadyCallback,
+        GoogleMap.OnMapLongClickListener {
+
+    private static final LatLng KansasCity = new LatLng(38.969273, -94.584664);
+
+    private DismissOverlayView mDismissOverlay;
+
+    private GoogleMap mMap;
+
+    private MapFragment mMapFragment;
+
+    public void onCreate(Bundle savedState) {
+        super.onCreate(savedState);
+
+        setContentView(R.layout.activity_main);
+
+        setAmbientEnabled();
+
+        final FrameLayout topFrameLayout = (FrameLayout) findViewById(R.id.root_container);
+        final FrameLayout mapFrameLayout = (FrameLayout) findViewById(R.id.map_container);
+
+        topFrameLayout.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
+            @Override
+            public WindowInsets onApplyWindowInsets(View v, WindowInsets insets) {
+                insets = topFrameLayout.onApplyWindowInsets(insets);
+
+                FrameLayout.LayoutParams params =
+                        (FrameLayout.LayoutParams) mapFrameLayout.getLayoutParams();
+
+                params.setMargins(
+                        insets.getSystemWindowInsetLeft(),
+                        insets.getSystemWindowInsetTop(),
+                        insets.getSystemWindowInsetRight(),
+                        insets.getSystemWindowInsetBottom());
+                mapFrameLayout.setLayoutParams(params);
+
+                return insets;
+            }
+        });
+
+        mDismissOverlay = (DismissOverlayView) findViewById(R.id.dismiss_overlay);
+        mDismissOverlay.setIntroText(R.string.intro_text);
+        mDismissOverlay.showIntroIfNecessary();
+
+        mMapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onEnterAmbient(Bundle ambientDetails) {
+        super.onEnterAmbient(ambientDetails);
+        mMapFragment.onEnterAmbient(ambientDetails);
+    }
+
+    @Override
+    public void onExitAmbient() {
+        super.onExitAmbient();
+        mMapFragment.onExitAmbient();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setOnMapLongClickListener(this);
+
+        mMap.addMarker(new MarkerOptions().position(KansasCity)
+                .title("Kansas City Holmes"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(KansasCity, 10));
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+
+        mDismissOverlay.show();
+    }
+}
---
+++ Lab Assignment 6/Source/README.md
@@ -0,0 +1,5 @@
+# Lab Assignment 6
+
+Here is the Wiki Page Github link for lab assignment 6
+
+https://github.com/sakiii999/ASE-Assignment/wiki/Lab-Assignment-6
